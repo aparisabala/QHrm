95f7262e0e410988c19100979f406079e292ffc0
Paris Ariyan <arianbala56@gmail.com>
2025-10-24 14:30:22 +0600

    employee login done

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -3,14 +3,50 @@
 namespace App\Models;
 
 use App\Traits\BaseTrait;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 //vpx_imports
 //crudDone
 class Employee extends Model
 {
-    use BaseTrait;
-    protected $table = "employees";
-    //vpx_attach
+    use HasApiTokens, HasFactory, Notifiable, BaseTrait;
+    protected $table = 'employees';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'mobile_numnber',
+        'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'password' => 'hashed',
+        'user_access' => 'array'
+    ];
+
     public function depertment()
     {
         return $this->hasOne(LibDepartment::class,'id','lib_department_id');
@@ -34,5 +70,10 @@ class Employee extends Model
     public function salary()
     {
         return $this->hasOne(EmployeeSalary::class,'employee_id','id');
+    }
+
+     public function hasPermission($permissions)
+    {
+        return (in_array('SA',$this->user_access)) ? true : count(array_intersect($this->user_access, $permissions)) > 0;
     }
 }
