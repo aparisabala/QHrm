d928891f4bf18852c75d753078bbbba301806dcc
Paris Ariyan <arianbala56@gmail.com>
2025-10-24 15:00:59 +0600

    added

---
+++ database/migrations/2025_10_24_145733_create_employee_attendance_histories_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('employee_attendance_histories', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('employee_attendance');
+            $table->string('type',25);
+            $table->string('title')->nullable();
+            $table->string('time',100)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('employee_attendance_histories');
+    }
+};
